5f209115ac499aa4c7571b70c98cc44d0a54910f
Arian Dostyar <dostyararian@gmail.com>
2025-08-20 02:58:02 -0700

    About page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Header from "./components/Header";
+import About from "./components/About";
 
 export default function App() {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<About />} />
         <Route path="/log-in" element={<Login />} />
         <Route
           path="/dashboard"
---
+++ client/src/components/About.jsx
@@ -0,0 +1,5 @@
+export default function About() {
+  return (
+    <div className="bg-gradient-to-br from-gray-900 via-slate-900 to-gray-800 min-h-screen flex items-center justify-center p-4"></div>
+  );
+}
