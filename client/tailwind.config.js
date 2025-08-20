f3bdd57178460e1a25da8a4c1b5cb873c051e759
Arian Dostyar <dostyararian@gmail.com>
2025-08-20 00:21:50 -0700

    ammend

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+        // Add custom font families
+        heading: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
