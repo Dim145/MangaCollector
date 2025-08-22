e619bac4cdb81f49fdf82b4df9aaa5b484f05b48
Arian Dostyar <dostyararian@gmail.com>
2025-08-22 16:05:06 -0700

    header text styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,10 +8,10 @@ export default function Header() {
 
   return (
     <nav className="bg-black border-b border-gray-800">
-      <div className="flex items justify-between px-6 py-4 mx-auto ">
+      <div className="flex justify-between px-6 py-4 mx-auto ">
         {/* Brand */}
         <a href="/" className="text-xl font-bold text-white no-underline">
-          <div className="flex gap-4">
+          <div className="flex items-end gap-4">
             <img src={Logo} className="max-w-8"></img>
             MangaCollector
           </div>
