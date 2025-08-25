05056e9051f98d9c16d0e0a2d5a15b0c598996c5
Arian Dostyar <dostyararian@gmail.com>
2025-08-25 15:49:21 -0700

    Added a wishlist button

--- client/src/components/ProfileButton.jsx
+++ client/src/components/ProfileButton.jsx
@@ -70,6 +70,14 @@ export default function ProfileButton() {
                 </li>
                 <li>
                   <button
+                    onClick={() => navigate("/wishlist")}
+                    className="w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-800"
+                  >
+                    Wishlist
+                  </button>
+                </li>
+                <li>
+                  <button
                     onClick={() => navigate("/dashboard")}
                     className="w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-800"
                   >
