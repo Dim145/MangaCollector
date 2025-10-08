3d61d7499a4f1cbe41fb2479fe6cdcf51a02c24c
dim145 <dimitri.bleach@gmail.com>
2025-10-08 21:33:11 +0200

    remove all static url references

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://mangacollector.onrender.com";
+axios.defaults.baseURL = process.env.FRONTEND_URL || document?.URL || "http://localhost:5173";
 axios.defaults.withCredentials = true;
 
 export default axios;
