06183a29dc7c183a22e8c713d677d0f28460df29
dim145 <dimitri.bleach@gmail.com>
2025-10-08 21:58:26 +0200

    fix origin url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const location = window?.location;
 
 axios.defaults.baseURL = (location ? location.origin : undefined) || "http://localhost:5173";
-console.log(document)
 axios.defaults.withCredentials = true;
 
 export default axios;
