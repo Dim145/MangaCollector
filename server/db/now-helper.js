67bc05aa0acf8609e6170c096c553f7e99bd113d
dim145 <dimitri.bleach@gmail.com>
2026-04-19 16:10:22 +0200

    chore: migrate to simpler server structure and remove legacy components
    Update server build context from `./server-rust` to `./server`, removing the Rust-based implementation and associated files. Clean up unused migrations, routes, services, and configuration files to streamline the codebase.

--- server/db/now-helper.js
+++
@@ -1,8 +0,0 @@
-const db     = require('./db');
-const Model  = require('objection').Model;
-
-Model.knex(db);
-
-module.exports = function () {
-    return Model.raw('NOW()');
-};
--- server/routes/volumeRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const volumeRouter = new Router();
-const volumeController = require("../controllers/volumeController");
-
-volumeRouter.get("/", volumeController.getAllVolumes);
-volumeRouter.get("/:mal_id", volumeController.getAllVolumesByID);
-volumeRouter.patch("/", volumeController.updateVolumeByID);
-
-module.exports = volumeRouter;
